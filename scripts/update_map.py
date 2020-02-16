c5e8815136477555d6187f8904db4c5204e8f3a7
vsoch <vsochat@stanford.edu>
2020-02-16 14:33:37 -0700

    adding workflow 1/2, this will run tests for current data
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- scripts/update_map.py
+++ scripts/update_map.py
@@ -6,7 +6,8 @@
 # Copyright @vsoch, 2020
 
 from geopy.geocoders import Nominatim
-#from uszipcode import SearchEngine
+
+# from uszipcode import SearchEngine
 
 import json
 import requests
@@ -22,24 +23,25 @@ import tempfile
 
 here = os.path.dirname(os.path.abspath(__file__))
 
+
 def get_filepath():
-    '''get path for the data file to write
-    '''
-    return os.path.join(os.path.dirname(here), '_data', 'locations.csv')
+    """get path for the data file to write
+    """
+    return os.path.join(os.path.dirname(here), "_data", "locations.csv")
 
 
 def get_lookup():
-    '''get path for the locations lookup file.
-    '''
-    filepath = os.path.join(os.path.dirname(here), '_data', 'location-lookup.tsv')
+    """get path for the locations lookup file.
+    """
+    filepath = os.path.join(os.path.dirname(here), "_data", "location-lookup.tsv")
     if not os.path.exists(filepath):
         sys.exit("Cannot find %s" % filepath)
     return filepath
 
 
-def read_rows(filepath, newline='', delim=','):
-    '''read in the data rows of a csv file.
-    '''
+def read_rows(filepath, newline="", delim=","):
+    """read in the data rows of a csv file.
+    """
     # Read in the entire membership counts
     with open(filepath, newline=newline) as infile:
         reader = csv.reader(infile, delimiter=delim)
@@ -48,20 +50,23 @@ def read_rows(filepath, newline='', delim=','):
 
 
 def main():
-    '''main entrypoint for the script to generate the locations file
-    '''
+    """main entrypoint for the script to generate the locations file
+    """
 
-    # A tsv download for just the worksheet with summary counts 
+    # A tsv download for just the worksheet with summary counts
     sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTBn_kgBH8WoFmqdRJYdw8GrmfvjbdWIMYCk-yxelaE8aUO3J0rY19_wPOI9HHW0U0tc5Bg19uApPzx/pub?gid=145007959&single=true&output=tsv"
 
     # Ensure the response is okay
     response = requests.get(sheet)
     if response.status_code != 200:
-        print('Error with getting sheet, response code %s: %s' %(response.status_code, response.reason))
+        print(
+            "Error with getting sheet, response code %s: %s"
+            % (response.status_code, response.reason)
+        )
         sys.exit(1)
 
     # Split lines by all sorts of fugly carriage returns
-    lines = response.text.split('\r\n')
+    lines = response.text.split("\r\n")
 
     # Remove empty responses, header row, make all lowercase
     lines = [x.lower().strip() for x in lines[1:] if x.strip()]
@@ -70,16 +75,16 @@ def main():
     lookup_rows = read_rows(get_lookup(), delim="\t")
 
     # Remove header (should have name and city-state)
-    assert lookup_rows[0][0] == 'name'
-    assert lookup_rows[0][1] == 'city-state'
+    assert lookup_rows[0][0] == "name"
+    assert lookup_rows[0][1] == "city-state"
     lookup_rows.pop(0)
 
     # Create lookup dictionary
-    lookup = {x[0]:x[1] for x in lookup_rows}
+    lookup = {x[0]: x[1] for x in lookup_rows}
 
     # Create geolocator and search engine
     geolocator = Nominatim(user_agent="us-rse.org")
-    #search = SearchEngine(simple_zipcode=True)
+    # search = SearchEngine(simple_zipcode=True)
 
     # Get lats/long for each location, keep track of missing
     latlong = dict()
@@ -89,9 +94,9 @@ def main():
         # Skip remote addresses
         if address == "remote":
             continue
-    
-        print("Looking up %s in %s" %(name, address))
-        # Second shot, try for international address           
+
+        print("Looking up %s in %s" % (name, address))
+        # Second shot, try for international address
 
         location = geolocator.geocode(address)
         if location:
@@ -99,9 +104,9 @@ def main():
             lng = location.longitude
             latlong[name] = [lat, lng]
         else:
-            print("%s: %s is not found with geocoding." %(name, address))
+            print("%s: %s is not found with geocoding." % (name, address))
             missing.add(name)
-   
+
     # Keep track of locations not known, counts known
     unknown = set()
     counts = {}
@@ -112,29 +117,29 @@ def main():
             continue
         if line not in counts:
             counts[line] = 0
-        counts[line] +=1
+        counts[line] += 1
 
     # Generate list of names with latitude and longitude for each
     # [name, lat, long, count]
-    locations = [['name', 'lat', 'lng', 'count']]
+    locations = [["name", "lat", "lng", "count"]]
     for name in lines:
- 
+
         # We found a location (lat long) for the place!
         if name in latlong and name in counts:
             locations.append([name, latlong[name][0], latlong[name][1], counts[name]])
 
-    print("Found a total of %s locations, each with a count!" % (len(locations) -1))
+    print("Found a total of %s locations, each with a count!" % (len(locations) - 1))
 
     # We will write results to this file
     filepath = get_filepath()
 
     # Write the new file
-    with open(filepath, 'w', newline='') as outfile:
-        writer = csv.writer(outfile, delimiter=',') 
+    with open(filepath, "w", newline="") as outfile:
+        writer = csv.writer(outfile, delimiter=",")
         [writer.writerow(row) for row in locations]
 
     print("Wrote locations and counts to %s." % filepath)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ tests/test_locations.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+# Author @vsoch, Written February 2020
+# Read in the locations.csv and location-lookup.tsv files to validate each entry
+
+import csv
+import os
+import pytest
+import re
+import requests
+from geopy.geocoders import Nominatim
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+def read_rows(filepath, newline="", delim=","):
+    """read in the data rows of a csv file.
+    """
+    # Read in the entire membership counts
+    with open(filepath, newline=newline) as infile:
+        reader = csv.reader(infile, delimiter=delim)
+        data = [row for row in reader]
+    return data
+
+
+def get_sheet_data():
+    """return Google sheets institution lookup
+    """
+    sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTBn_kgBH8WoFmqdRJYdw8GrmfvjbdWIMYCk-yxelaE8aUO3J0rY19_wPOI9HHW0U0tc5Bg19uApPzx/pub?gid=145007959&single=true&output=tsv"
+
+    # Ensure the response is okay
+    response = requests.get(sheet)
+    if response.status_code != 200:
+        print(
+            "Error with getting sheet, response code %s: %s"
+            % (response.status_code, response.reason)
+        )
+        sys.exit(1)
+
+    # Split lines by all sorts of fugly carriage returns
+    lines = response.text.split("\r\n")
+
+    # Remove empty responses, header row, make all lowercase
+    return [x.lower().strip() for x in lines[1:] if x.strip()]
+
+
+def test_locations_lookup(tmp_path):
+    """The locations-lookup.tsv file is a tab separated file of locations.
+       Specifically, we need to ensure that it's in alphabetical order,
+       and each has a city state that returns a location.
+       The only other option is for a location to be remote, in which case,
+       it is skipped. 
+    """
+    filepath = os.path.join(os.path.dirname(here), "_data", "location-lookup.tsv")
+    assert os.path.exists(filepath)
+    lines = read_rows(filepath, delim="\t")
+
+    # These are to skip, don't know what they mean
+    skips = ["llnsâ\\x80¯llc"]
+
+    # 1. Ensure header is correct
+    print("1. Testing for correct header with 'name' and 'city-state'")
+    assert lines[0][0] == "name"
+    assert lines[0][1] == "city-state"
+
+    # 2. must be all lowercase, in abc order
+    print("2. Names must be all lowercase and in alphabetical order.")
+    names = [x[0].lower() for x in lines[1:]]
+    comparator = list(set(x[0] for x in lines[1:]))
+    comparator.sort()
+    for i in range(len(names)):
+        assert comparator[i] == names[i]
+
+    # 3. All locations in lookup must be in actual data
+    print("3. All locations in lookup must be in actual data.")
+    data = get_sheet_data()
+    for line in lines[1:]:
+        name = line[0]
+        if name in skips:
+            continue
+        assert name in data
+
+    # 4. Ensure that each location has a lat and long
+    print("4. Testing that each location has a latitude and longitude.")
+    geolocator = Nominatim(user_agent="us-rse.org")
+    for line in lines[1:]:
+        name = line[0]
+        address = line[1]
+        print("Testing %s" % name)
+
+        if name in skips:
+            continue
+
+        if address == "remote":
+            print("%s is considered remote." % address)
+            continue
+
+        location = geolocator.geocode(address)
+        assert location
+
+    print("*Use preview on CircleCI to ensure that locations found are correct*")
+
+
+def test_locations(tmp_path):
+    """The locations.csv file is a comma separated file of locations.
+       Each should have a name (lowercase), latitude, and longitude.
+    """
+    filepath = os.path.join(os.path.dirname(here), "_data", "locations.csv")
+    assert os.path.exists(filepath)
+    lines = read_rows(filepath)
+
+    # 1. Check that header is correct
+    print("1. Checking that header has name, lat, lng, count")
+    header = lines.pop(0)
+    assert header[0] == "name"
+    assert header[1] == "lat"
+    assert header[2] == "lng"
+    assert header[3] == "count"
+
+    # 2. Check that name is all lowercase
+    print("Checking names, latitudes, longitudes, and counts != 0")
+    for line in lines:
+        name = line[0]
+        lat = line[1]
+        lng = line[2]
+        count = line[3]
+
+        print("Testing %s" % name)
+        assert name.lower() == name
+        assert float(lat)
+        assert float(lng)
+        assert int(count)
+        assert int(count) != 0
